d29cad5d67bd759a9580abffb25c1fd5ee634885
Yangseungchan <--uvzone@naver.com>
2020-09-19 18:15:05 +0900

    2108 Update(Solved)

--- DynamicProgramming/2108/2108.cpp
+++ DynamicProgramming/2108/2108.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 int index(int i)
 {
-  return i+4000;
+  return i + 4000;
 }
 
 void print_vector(vector<int> &ref)
 {
-  for(int i=0; i<ref.size(); i++)
+  for (int i = 0; i < ref.size(); i++)
   {
     cout << ref[i] << " ";
   }
@@ -21,65 +22,53 @@ void print_vector(vector<int> &ref)
 int average(int sum, int count)
 {
   double avg_d = static_cast<double>(sum) / static_cast<double>(count);
-
-  int avg_i = sum / count;
-
-  if(avg_d - avg_i > 0.5)
-    return (sum / count) + 1;
-  return sum / count;
+  return round(avg_d);
 }
 
-int middle_value(vector<int> container, int length)
+int middle_value(vector<int> &container, int length, int min, int max)
 {
   int count = (length / 2) + 1;
-  for(int i=0; i<=8000; i++)
+  for (int i = index(min); i <= index(max); i++)
   {
-    if(container[i] ==0)
+    if (container[i] == 0)
       continue;
     count -= container[i];
-    if(count <= 0)
+    if (count <= 0)
     {
-      return i-4000;
+      return i - 4000;
     }
   }
+  return -1;
 }
 
-int most_frequent(vector<int> container)
+int most_frequent(vector<int> &container, int container_min, int container_max)
 {
   int min;
   int max = 0;
   int max_idx;
   vector<int> arr;
-  for(int i=0; i<= container.size(); i++)
+  for (int i = index(container_min); i <= index(container_max); i++)
   {
-    if(container[i] > max)
+    if (container[i] > max)
     {
       arr.clear();
+      arr.push_back(i); // arr에는 index함수에 의해서 변환된 index(물리적으로 저장되는 인덱스)가 저장
       max = container[i];
       max_idx = i;
     }
-    if(container[i] == max)
+    else if (container[i] == max)
     {
-      arr.push_back(max_idx);
+      arr.push_back(i);
     }
   }
-  if(arr.size() > 1)
+  if (arr.size() > 1)
   {
     sort(arr.begin(), arr.end());
-    print_vector(arr);
-    min = arr[0];
-    for(int i=0; i<arr.size(); i++)
-    {
-      if(min < arr[i])
-      {
-        return arr[i];
-      }
-    }
+    return arr[1] - 4000;
   }
 
   return max_idx - 4000;
 }
-
 
 int main(void)
 {
@@ -88,28 +77,26 @@ int main(void)
 
   int sum, min, max, num, length;
   vector<int> container(8001, 0); //index : 0 ~ 8001(-4000~4000)
-  
+
   cin >> length;
   sum = 0;
   min = 9999;
   max = -9999;
-  for(int i=0; i<length; i++)
+  for (int i = 0; i < length; i++)
   {
     cin >> num;
     sum += num;
     container[index(num)] += 1;
-    if(min > num)
+    if (min > num)
       min = num;
-    if(max < num)
+    if (max < num)
       max = num;
   }
 
-  cout << average(sum , length) << endl; // average
-  cout  << middle_value(container, length) << endl;// middle value 
-  cout  << most_frequent(container) << endl; // most frequent value
-  cout << max - min << endl; // range
-
+  cout << average(sum, length) << '\n';                      // average
+  cout << middle_value(container, length, min, max) << '\n'; // middle value
+  cout << most_frequent(container, min, max) << '\n';        // most frequent value
+  cout << max - min << '\n';                                 // range
 
   return 0;
 }
-
