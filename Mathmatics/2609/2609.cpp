653bb19dcd47dd3e2a6326e9a078bbe07c3388eb
Yangseungchan <uvzone@naver.com>
2020-12-03 16:21:40 +0900

    Update 2609 solution

--- Mathmatics/2609/2609.cpp
+++ Mathmatics/2609/2609.cpp
@@ -1,72 +1,26 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
 
 using namespace std;
 
-void find_factor(int num, vector<int> &arr, int &max)
+int gcd(int n1, int n2)
 {
-    int i;
-    for (i = 2; i * i <= num; i++)
+    if (n2 == 0)
     {
-        while (num % i == 0)
-        {
-            num /= i;
-            arr[i] += 1;
-            if (max < i)
-                max = i;
-        }
+        return n1;
     }
-    if (num > 1)
-    {
-        arr[num] += 1;
-        if (max < num)
-            max = num;
-    }
-}
-
-void print_factor(vector<int> &arr)
-{
-    for (int i = 2; i < arr.size(); i++)
-    {
-        if (arr[i] > 0)
-        {
-            cout << i << " : " << arr[i] << '\n';
-        }
-    }
+    return gcd(n2, n1 % n2);
 }
 
 int main(void)
 {
     cin.tie(NULL);
+    cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    vector<int> arr1;
-    vector<int> arr2;
-
-    int num1, num2, max1, max2, min_max, gcd, lcm;
-    cin >> num1 >> num2;
-
-    arr1.assign(num1 + 1, 0);
-    arr2.assign(num2 + 1, 0);
-
-    max1 = max2 = 0;
-    find_factor(num1, arr1, max1);
-    find_factor(num2, arr2, max2);
-    min_max = max(max1, max2);
-
-    gcd = lcm = 1;
-    for (int i = 2; i <= min_max; i++)
-    {
-        if (arr1[i] > 0 || arr2[i] > 0)
-        {
-            gcd *= (int)pow(i, min(arr1[i], arr2[i]));
-            lcm *= (int)pow(i, max(arr1[i], arr2[i]));
-        }
-    }
-    cout << gcd << '\n';
-    cout << lcm << '\n';
-
+    int n1, n2, temp;
+    cin >> n1 >> n2;
+    temp = gcd(n1, n2);
+    cout << temp << '\n';
+    cout << n1 * n2 / temp << '\n';
     return 0;
 }
