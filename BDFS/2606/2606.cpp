de3dd074c9fde2a908dfb61295590a443357078f
Yangseungchan <uvzone@naver.com>
2021-01-25 17:30:57 +0900

    change 2606 source codes

--- BDFS/2606/2606.cpp
+++ BDFS/2606/2606.cpp
@@ -1,110 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-// #include <cstdio>
-#include <algorithm>
+#include <cstdlib>
 
 using namespace std;
 
-typedef struct __pos
+typedef struct __node
 {
-    int x;
-    int y;
-} Pos;
+    int data;
+    struct __node *next;
+} Node;
 
-vector<vector<bool>> visit; // bfs상에서의 방문 여부확인용
-vector<vector<bool>> done;  // bfs_1 상에서의 1들에 대한 방문 여부 확인용
-vector<vector<int>> graph;  // 실제 그래프
-vector<int> results;        // 1탐색 결과를 담아내는 벡터
+Node **graph;
+int result;
 
-queue<Pos> bfs_queue;
-queue<Pos> bfs_1_queue;
+vector<bool> visit;
+queue<int> bfs_queue;
 
-void bfs_1(int size) // 1 방문시 done에 기록
-{
-    int count1 = 0;
-    while (!bfs_1_queue.empty())
-    {
-        Pos index = bfs_1_queue.front();
-        bfs_1_queue.pop();
-        count1++;
-
-        if (index.x + 1 < size)
-        {
-            Pos index1 = {index.x + 1, index.y};
-            if (graph[index1.x][index1.y] == 1 && done[index1.x][index1.y] == false)
-            {
-                done[index1.x][index1.y] = true;
-                bfs_1_queue.push(index1);
-            }
-        }
-        if (index.y + 1 < size)
-        {
-            Pos index2 = {index.x, index.y + 1};
-            if (graph[index2.x][index2.y] == 1 && done[index2.x][index2.y] == false)
-            {
-                done[index2.x][index2.y] = true;
-                bfs_1_queue.push(index2);
-            }
-        }
-        if (index.x - 1 >= 0)
-        {
-            Pos index3 = {index.x - 1, index.y};
-            if (graph[index3.x][index3.y] == 1 && done[index3.x][index3.y] == false)
-            {
-                done[index3.x][index3.y] = true;
-                bfs_1_queue.push(index3);
-            }
-        }
-        if (index.y - 1 >= 0)
-        {
-            Pos index4 = {index.x, index.y - 1};
-            if (graph[index4.x][index4.y] == 1 && done[index4.x][index4.y] == false)
-            {
-                done[index4.x][index4.y] = true;
-                bfs_1_queue.push(index4);
-            }
-        }
-    }
-    results.push_back(count1);
-}
-
-void bfs(int size)
+void bfs()
 {
     while (!bfs_queue.empty())
     {
-        Pos index = bfs_queue.front();
+        int index = bfs_queue.front();
+        // cout << index << "-> ";
         bfs_queue.pop();
-
-        if (index.x + 1 < size)
+        Node *nde = graph[index];
+        while (nde != NULL)
         {
-            Pos index1 = Pos{index.x + 1, index.y};
-            if (visit[index1.x][index1.y] == false)
+            int data = nde->data;
+            if (visit[data] == false)
             {
-                if (graph[index1.x][index1.y] == 1 && done[index1.x][index1.y] == false)
-                {
-                    bfs_1_queue.push(index1);
-                    done[index1.x][index1.y] = true;
-                    bfs_1(size);
-                }
-                bfs_queue.push(index1);
-                visit[index1.x][index1.y] = true;
+                bfs_queue.push(data);
+                result++;
+                visit[data] = true;
             }
-        }
-        if (index.y + 1 < size)
-        {
-            Pos index2 = Pos{index.x, index.y + 1};
-            if (visit[index2.x][index2.y] == false)
-            {
-                if (graph[index2.x][index2.y] == 1 && done[index2.x][index2.y] == false)
-                {
-                    bfs_1_queue.push(index2);
-                    done[index2.x][index2.y] = true;
-                    bfs_1(size);
-                }
-                bfs_queue.push(index2);
-                visit[index2.x][index2.y] = true;
-            }
+            nde = nde->next;
         }
     }
 }
@@ -115,46 +45,35 @@ int main(void)
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    char ch;
-    int N;
-    cin >> N;
-
-    graph = vector<vector<int>>(N, vector<int>(N));
-    visit = vector<vector<bool>>(N, vector<bool>(N, false));
-    done = visit;
-    for (int i = 0; i < N; i++)
+    int V, E, from, to; // Vertex = 1~V
+    cin >> V >> E;
+    result = 0;
+    visit = vector<bool>(V + 1, false);
+    graph = (Node **)malloc(sizeof(Node *) * (V + 1));
+    for (int i = 1; i <= V; i++)
     {
-        for (int j = 0; j < N; j++)
-        {
-            cin >> ch;
-            if (ch == '\n')
-            {
-                // skip;
-                j--;
-                continue;
-            }
-            int num = ch - '0';
-            graph[i][j] = num;
-        }
+        graph[i] = NULL;
     }
-    Pos index = {0, 0};
-    visit[0][0] = true;
-    bfs_queue.push(index);
-    if (graph[0][0] == 1)
+    // 포인터는 = 의 왼쪽에 있냐 오른쪽에 있냐에 따라 역할이 달라진다
+    // 왼쪽 => 주솟값을 나타내고, 오른쪽 => 가리키는 대상을 나타낸다
+    for (int i = 0; i < E; i++)
     {
-        Pos index = {0, 0};
-        bfs_1_queue.push(index);
-        done[0][0] = true;
-        bfs_1(N);
-    }
-    bfs(N);
+        cin >> from >> to;
+        Node *nde = (Node *)malloc(sizeof(Node));
+        nde->data = to;
+        nde->next = graph[from];
+        graph[from] = nde;
 
-    sort(results.begin(), results.end());
-    cout << results.size() << '\n';
-    for (int i = 0; i < results.size(); i++)
-    {
-        cout << results[i] << '\n';
+        Node *nde2 = (Node *)malloc(sizeof(Node));
+        nde2->data = from;
+        nde2->next = graph[to];
+        graph[to] = nde2;
     }
+    visit[1] = true;
+    bfs_queue.push(1);
+    bfs();
+    // cout << endl;
 
+    cout << result << '\n';
     return 0;
 }
